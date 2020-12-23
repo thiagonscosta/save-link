757515e675475f40bb0c23710244509b0e6802a9
ThiagoCosta <thiagodacosta@great.ufc.br>
2020-12-23 16:22:15 -0300

    fix endpoint

--- app/route/article.js
+++ app/route/article.js
@@ -2,5 +2,5 @@ const articleController = require('../controller/article.js');
 
 module.exports = function (app) {
   app.post('/api/article/create', articleController.create);
-  app.get('/api/articles', articleController.getAll);
+  app.get('/api/article/all', articleController.getAll);
 };
